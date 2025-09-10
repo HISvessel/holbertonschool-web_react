f3625084db3e8bf3cc0e71bf04ea51c2a4a1ecea
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-10 15:06:46 -0400

    added changes

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -10,7 +10,7 @@ const student1: Student = {
   lastName: "",
   age: 0,
   location: ""
-} ;
+};
 const student2: Student = {
   firstName: '',
   lastName: '',
@@ -20,7 +20,13 @@ const student2: Student = {
 
 const studentList = [student1, student2];
 
-studentList.forEach(student => {
-  student.firstName,
-  student.location
+const studentTable = document.createElement('table');
+studentTable.insertRow();
+
+const studentName = document.createElement('row');
+const studentLoc = document.createElement('row');
+
+const nameListObject = studentList.forEach(student => {
+  const name = student.firstName
+  const location = student.location
 });
