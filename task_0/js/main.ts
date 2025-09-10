6da80d52e7021dedcb7dd0a170601fb8bde449a0
HISvessel <kevinsanchezdabest@yahoo.com>
2025-09-10 00:34:11 -0400

    utilizing typescript for type safety with vanilla JavaScript

---
+++ task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+};
+
+const student1: Student = {
+  firstName: "",
+  lastName: "",
+  age: 0,
+  location: ""
+} ;
+const student2: Student = {
+  firstName: '',
+  lastName: '',
+  age: 0,
+  location: '',
+};
+
+const studentList = [student1, student2];
